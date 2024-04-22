7182a28edbeac465ae0478e3c18314236d170561
Clyde <clyde.autin@gmail.com>
2024-04-22 14:16:24 -0700

    test created for model class

---
+++ spec/model_spec.rb
@@ -0,0 +1,15 @@
+require './lib/model.rb'
+require 'pry'
+
+# frozen_string_literal: true
+RSpec.describe Model do
+
+    describe 'it exists' do
+        uriel_v = Model.new('Uriel Ventris', 6, 4, 3, 4, 5, 6, 1)
+
+        expect(uriel_v).to be_an_instance_of(Model)
+    end
+
+end
+
+
