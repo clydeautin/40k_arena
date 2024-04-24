7cf286717dd22d5ec8deea44eaef87abd5a7d5a3
Clyde <clyde.autin@gmail.com>
2024-04-24 11:06:56 -0700

    Ranged weapon shooting method is almost complete

--- lib/ranged_weapon.rb
+++ lib/ranged_weapon.rb
@@ -20,12 +20,52 @@ class Ranged_weapon
         @abilities = []
     end
 
-    def shoot
+    def shoot(target)
         hits = 0
         @attacks.times do
             roll = rand(1..6)  # Simulate a d6 roll
-            its += 1 if roll > @bs
+            hits += 1 if roll > @bs
         end
-        hits
+        if hits > 0
+            wound_rolls = wound_roll(hits)
+            wounds = check_wounds(wound_rolls, target)
+            saved_wounds = armore_saves(wounds, target)
+            final_wounds = wounds - saved_wounds
+
+            final_wounds.times { target.wound }
+            return final_wounds 
+        else
+            return 0
+        end
+    end
+
+    def wound_roll(hits)
+        hits.times.map { rand(1..6) }
+    end
+
+    def wound_roll(hits)
+        wound_needed = wound_check(target)
+        wound_rolls.count do |roll|
+            roll >= wound_needed
+        end
+    end
+
+    def wound_check(target)
+        if @strength >= target.toughness * 2
+            return 2
+        elsif @strength > target.toughness
+            return 3
+        elsif @strength == target.toughness
+            return 4
+        elsif @strength >= target.toughness / 2
+            return 5
+        else
+            return 6
+        end
+    end
+
+    def armor_saves(wounds, target)
+        saves_required = [target.save + @ap, target.invul_save].compact.min
+        wounds.times.count { rand(1..6) >= saves_required }
     end
 end
--- spec/ranged_weapon_spec.rb
+++ spec/ranged_weapon_spec.rb
@@ -2,6 +2,11 @@ require './spec/spec_helper'
 
 
 RSpec.describe Ranged_weapon do
+    let(:heavy_boltgun) { Ranged_weapon.new('Heavy Boltgun', 24, 2, 3, 4, -1, 1) }
+    let(:plasma_pistol) { Ranged_weapon.new('Plasma Gun', 12, 1, 3, 7, -2, 1) }
+    let(:bolt_p2) { Ranged_weapon.new('Bolt Pistol', 12, 1, 2, 4, 0, 1, 'Pistol') }
+    let(:cultist) {Model.new('Cultist Mob', 6, 3, 6, nil, 1, 7, 1)}
+    let(:terminator) {Model.new('Terminator', 5, 5, 6, 4, 3, 6, 1)}
 
     describe '#initialize' do
         it 'is an instance of Ranged_weapon' do
@@ -14,7 +19,8 @@ RSpec.describe Ranged_weapon do
     describe '#attack' do
         it 'a weapon should be able to shoot' do
             bolt_p2 = Ranged_weapon.new('Bolt Pistol', 12, 1, 2, 4, 0, 1, 'Pistol')
-            hits = bolt_p2.shoot
+            cultist = Model.new('Cultist Mob', 6, 3, 6, nil, 1, 7, 1)
+            hits = bolt_p2.shoot(cultist)
 
             expect(hits).to be_between(0, 1).inclusive
         end
@@ -22,7 +28,8 @@ RSpec.describe Ranged_weapon do
         it 'a weapon should be able to wound' do
             bolt_p2 = Ranged_weapon.new('Bolt Pistol', 12, 1, 2, 4, 0, 1, 'Pistol')
             cultist = Model.new('Cultist Mob', 6, 3, 6, nil, 1, 7, 1)
-            hits = bolt_p2.shoot
+            hits = bolt_p2.shoot(cultist)
+
 
             expect(hits).to be_between(0, 1).inclusive
         end
